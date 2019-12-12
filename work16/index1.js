49e746a18161f27378156d84be374195e9b34fcc
SV-N9 <1378426452@qq.com>
2019-12-12 13:34:48 +0800

    work16

--- work16/index1.js
+++ work16/index1.js
@@ -1,34 +1,31 @@
-function animate(obj, option) {
-    clearInterval(obj.timer)
-    obj.timer = setInterval(function() {
-        var flag = true;
-        for(var k in option) {
-            var leader = parseInt(getStyle(obj,k)) || 0;
-            var target = option[k];
-            var step = (target - leader) / 10;
-            step = step > 0 ? Math.ceil(step) : Math.floor(step);
-            leader = leader + step;
-            obj.style[k] = leader + 'px';
-            if (leader != target) {
-                flag = false;
+function animate(obj, option){
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function(){
+        var flag=true;
+        for(var k in option){
+            var leader=parseInt(getStyle(obj,k))||0;
+            var target=option[k];
+            var step=(target-leader)/10;
+            step=step>0?Math.ceil(step):Math.floor(step);
+            leader=leader+step;
+            obj.style[k]=leader+'px';
+            if(leader!=target){
+                flag=false;
             }
         }
-        if (flag) {
+        if(flag){
             clearInterval(obj.timer);
         }
-    }, 15);
+    },15);
 }
-document.getElementById('box').innerHTML = "肉肉肉快跑";
-function getStyle(obj, attr) {
-    if (window.getComputedStyle) {
-         return window.getComputedStyle(obj, null)[attr];
-    } else {
+function getStyle(obj,attr){
+    if(window.getComputedStyle){
+        return window.getComputedStyle(obj,null)[attr];
+    }else{
         return obj.currentStyle[attr];
     }
 }
-var obj = document.getElementById('box');
-obj.onclick = function() {
-    var left = parseInt(Math.random()*window.innerWidth);
-    var top = parseInt(Math.random()*window.innerHeight);
-    animate(obj, {'left':left,'top':top});
+var obj=document.getElementById('box');
+obj.onclick=function(){
+    animate(obj,{'left':200,'top':50});
 };
